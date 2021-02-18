fa3e7c7ca2f5075eda3c69efe3403b7dfb6f478a
Shubhu Shahade <shubhankarmandar.shahade@sjsu.edu>
2021-02-18 16:57:04 +0530

    Radio group pending

--- app/src/main/java/edu/sjsu/shubhushahade/project1shubhushahade/MainActivity.java
+++ app/src/main/java/edu/sjsu/shubhushahade/project1shubhushahade/MainActivity.java
@@ -4,18 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText PrincipleAmt;
     private EditText Result;
-    private EditText RatePercent;
 
     private TextView Principle;
     private TextView loanTerm;
@@ -31,23 +32,74 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox checkBox;
     private SeekBar seekBar;
 
+    float intRate = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PrincipleAmt = findViewById(R.id.PrincipleAmt);
+        PrincipleAmt = (EditText) findViewById(R.id.PrincipleAmt);
         Result = findViewById(R.id.Result);
-        RatePercent= findViewById(R.id.RatePercent);
-        Principle= findViewById(R.id.Principle);
+        Principle = findViewById(R.id.Principle);
         loanTerm = findViewById(R.id.loanTerm);
         interestRate = findViewById(R.id.interestRate);
-        y15 = findViewById(R.id.y15);
-        y20 = findViewById(R.id.y20);
-        y30 = findViewById(R.id.y30);
+        y15 = (RadioButton) findViewById(R.id.y15);
+        y20 = (RadioButton) findViewById(R.id.y20);
+        y30 = (RadioButton) findViewById(R.id.y30);
         calculate = findViewById(R.id.Calculate);
-        uninstall= findViewById(R.id.Uninstall);
-        checkBox= findViewById(R.id.checkBox);
+        uninstall = findViewById(R.id.Uninstall);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
         seekBar = findViewById(R.id.seekBar);
+        setSeekBar();
+        }
+
+
+    public void setSeekBar(){
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                intRate = progress;
+                interestRate.setText("Interest rate: " + (float) progress / 10);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                interestRate.setText("Interest rate: " + (float) intRate / 10);
+            }
+        });
     }
+
+    public void Calculate(View view){
+        float T = 0;
+        float M = 0;
+        float N = 12;
+        if (PrincipleAmt.getText().length() == 0) {
+            Toast.makeText(this, "Please enter a valid number",
+                    Toast.LENGTH_LONG).show();
+            return; }
+        float P = Float.parseFloat(PrincipleAmt.getText().toString());
+        if (checkBox.isChecked() == true){
+            T = (float) ((0.1/100) * P);
+        }
+        else if (checkBox.isChecked() == false){
+            T = 0;
+        }
+
+        if (intRate > 0){
+            M = P/N * T;
+        }
+        else {
+
+        }
+
+        Result.setText(String.valueOf(M));
+    }
+
 }
